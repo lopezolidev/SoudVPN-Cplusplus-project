d11d50e132fd54760ed714249d9b427f4a90d905
rockwatch <rocksistor@gmail.com>
2023-03-17 06:26:28 -0400

    [ADD] adding limbo functiona and C proc

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 //     return;
 // }
 
+
 bool isItPrime(int p){
     if(p == 2 || p == 3 || p == 5 ||p == 7 ||p == 11 ||p == 13 ||p == 17 ||p == 19 ||p == 23 ||p == 29 ||p == 31 || p == 37 || p == 41 || p == 43 || p == 47 ||p == 53 ||p == 59 ||p == 61 ||p == 67 || p == 71 || p == 73 || p == 79 || p == 83 || p == 89 || p == 97 || p == 67){
         cout << "Prime";
@@ -21,6 +22,75 @@ bool isItPrime(int p){
 long int correction(long int n){
     if(n < 0) return n * -1;
     return n;
+}
+
+long int flip(long int n){
+    int v1, v2, v3, v4, v5;
+    v1 = n % 100;
+    n /= 100;
+
+    v2 = n % 100;
+    n /= 100;
+    
+    v3 = n % 100;
+    n /= 100;
+    
+    v4 = n % 100;
+    n /= 100;
+    
+    v5 = n;
+        
+    return v1*100000000 + v2*1000000 + v3*10000 + v4*100 + v5;    
+}
+
+long int funC( long int n){
+    int v1, v2, v3, v4, v5;
+    
+    n = flip(n);
+
+    v1 = n % 100;
+    n /= 100;
+
+    v2 = n % 100;
+    n /= 100;
+    
+    v3 = n % 100;
+    n /= 100;
+    
+    v4 = n % 100;
+    n /= 100;
+    
+    v5 = n;
+
+        funA(v1);
+        funA(v2);
+        funA(v3);
+        funA(v4);
+    //calling function A, either way is going to execute the same code, with the exception of flipping
+
+    if(v5 != 0){
+        funA(v5);
+    } 
+}
+
+long int funA( long int n){
+
+}
+
+
+long int funB( long int n){
+    
+}
+
+void limbo(long int n){
+    if(isItPrime(n)){
+        funC(n);
+    } else if(n > 0) {
+        funA(n);
+    } else {
+        funB(n);
+    }
+    return;
 }
 
 int main(){
@@ -44,15 +114,10 @@ int main(){
         return -1;
     }
 
-    cout << first % 100;
+    limbo(first);
+    limbo(second);
 
-    if(isItPrime(first % 100)){
-        cout << "Hello!";
-    } else {
-        cout << "Nope!";
-    }
-    //calling prime number function to call procedure C
-
+    //calling limbo function to call procedure A, B or C
 
     return 0;
 }
